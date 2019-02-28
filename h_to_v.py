1eba9b74b5cd0e120a0866ac9562a56321be7eae
neroksi <32114038+neroksi@users.noreply.github.com>
2019-02-28 21:17:12 +0100

    Create h_to_v.py

---
+++ h_to_v.py
@@ -0,0 +1,24 @@
+def h_to_v(imgs):
+    imVs = []
+    imHs = []
+    for  img in imgs :
+        img = img.copy()
+        if img[1] == "V":
+            imVs.append(img)
+        else :
+            imHs.append(img)
+
+    idH = len(imHs)
+    idV = len(imVs)
+
+    k = idH
+    L = []
+    for i in range(idV):
+        for j in range(idV):
+            if i != j :
+                img1 = imVs[i]
+                img2 = imVs[j]
+                img = merge_vertical(img1, img2)
+                L.append(img)
+            
+    return imHs + L
